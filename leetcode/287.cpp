0e1de4c63a9f07d943c0c6f622e297e319cbc8de
zhusongquan <zhusongquan@kuaishou.com>
2021-01-04 17:17:15 +0800

    2021-01-04, binary-search in value

---
+++ leetcode/287.cpp
@@ -0,0 +1,44 @@
+
+// 二分
+//
+//  cnt_value <= value
+//  1 1 1 1 1 1 0 0 0 0 0 0
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int l = 1, r = n - 1, ans = -1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            int cnt = 0;
+            for (int i = 0; i < n; ++i) {
+                cnt += nums[i] <= mid;
+            }
+            if (cnt <= mid) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+};
+
+// 快慢指针
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
